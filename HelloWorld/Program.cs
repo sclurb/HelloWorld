1957f24bf10d74c1def34bc66957b2cd1e3c4108
Bob <Bob@BOBZILLA_16>
2017-12-12 11:01:35 -0500

    Entered a simple while loop with an If satement

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -10,9 +10,24 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
-
+            int x = 1;
             Console.WriteLine("Hello World");
             Console.WriteLine("I have now done an update on my work Computer!");
+            while (x == 1)
+            {
+
+            
+                string feldercarp = Console.ReadLine();
+                if (feldercarp == "Hey You")
+                {
+                    Console.WriteLine("You have a match.");
+                    x = 0;
+                }
+                else
+                {
+                    Console.WriteLine("Please Try again");
+                }
+            }
 
             Console.ReadKey();
         }
